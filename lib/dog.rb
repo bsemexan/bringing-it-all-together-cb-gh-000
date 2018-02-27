ef78058f285c5f5a2c0a06270d389071896daff9
Berry  Semexan <bsemexan@gmail.com>
2018-02-27 16:46:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,4 +44,10 @@ class Dog
     dogs
   end
 
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Song.new(result[0], result[1], result[2])
+  end
+
 end
